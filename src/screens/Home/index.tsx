2a3eb5ddd13648ea3b0a60031023a74b46f7e767
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-08 12:11:48 -0300

    Removed Comments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -53,13 +53,10 @@ export function Home() {
   const theme = useTheme()
   const { signOut, user, isLoggingOut } = useAuth()
 
-  // creates a function to handle sign out
   function handleSignOut() {
-    // try to call and wait signOut
     try {
       signOut()
     } catch (error) {
-      // if fails, display an Alert with the title "Erro SignOut" and message "Ocorreu um erro ao tentar se deslogar do app"
       Alert.alert(
         'Erro SignOut',
         'Ocorreu um erro ao tentar se deslogar do app'
@@ -156,10 +153,7 @@ export function Home() {
           </UserInfoText>
         </UserInfo>
 
-        <SignOutButton onPress={}>
-          {/* Verify if isLoggingOut is true
-          If it is, show an ActivityIndicator
-          Otherwise, show Feather's power icon */}
+        <SignOutButton onPress={handleSignOut}>
           {isLoggingOut ? (
             <ActivityIndicator size={25} color={theme.colors.white} />
           ) : (
